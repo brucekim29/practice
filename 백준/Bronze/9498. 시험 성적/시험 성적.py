f1580ebe6df99da51feabdc4cb95f9a6fb8b8535
Bruce Kim <112926997+brucekim29@users.noreply.github.com>
2022-10-14 18:18:36 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a>=90 or a==100:
+    print('A')
+elif a<=89 and a>=80:
+    print('B')
+elif a<=79 and a>=70:
+    print('C')
+elif a<=69 and a>=60:
+    print('D')
+elif a < 60:
+    print('F')
